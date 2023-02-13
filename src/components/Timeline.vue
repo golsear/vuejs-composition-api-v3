a9e7ddf4d77696d6ad210bf68c98d939fd85fec8
Serhii Holub <serhii.holub@UA02273.local>
2023-02-13 15:48:00 +0200

    Make variable reactive with ref and click event

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+import { ref } from "vue"
+
 const periods = ["Today", "This Week", "This Month"]
 
-function foo () {return 'foo'}
+const selectedPeriod = ref("Today")
+
+function selectPeriod (period: string) {
+    console.log(period)
+    selectedPeriod.value = period
+}
 </script>
 <template>
+    {{ selectedPeriod }}
     <nav class="is-primary panel">
         <span class="panel-tabs">
             <a v-for="period of periods"
                :key="period"
+               @click="selectPeriod(period)"
             >
                 {{ period }}
             </a>
