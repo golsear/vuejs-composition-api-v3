a490b00352374a039a20dab52571a6ff670a8cb8
Serhii Holub <serhii.holub@UA02273.local>
2023-02-16 16:24:29 +0200

    State Data Structures

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -18,8 +18,14 @@ function selectPeriod (period: Period) {
 }
 
 const posts = computed<TimelinePost[]>(() => {
-        return [today, thisWeek, thisMonth]
-                .map(post => {  
+        return postsStore.ids
+                .map(id => {  
+                    const post = postsStore.all.get(id)
+
+                    if (!post) {
+                        throw Error(`Post with id of ${id} was expected but not found.`)
+                    }
+
                     return {
                         ...post,
                         created: DateTime.fromISO(post.created)
