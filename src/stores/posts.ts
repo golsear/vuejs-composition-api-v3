829073f7e0647c02b29981dcc4bfa614495205c1
Serhii Holub <serhii.holub@UA02273.local>
2023-02-20 10:46:45 +0200

    Extracting Complexity

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,9 +1,12 @@
 import { defineStore } from "pinia"
-import { Post, today, thisWeek, thisMonth } from "../posts"
+import { Post, today, thisWeek, thisMonth, TimelinePost } from "../posts"
+import { Period } from "../constans"
+import { DateTime } from "luxon"
 
 interface PostState {
     ids: string[],
-    all: Map<string, Post>
+    all: Map<string, Post>,
+    selectedPeriod: Period
 }
 
 export const usePosts = defineStore("posts", {
@@ -13,10 +16,42 @@ export const usePosts = defineStore("posts", {
             [today.id, today],
             [thisWeek.id, thisWeek],
             [thisMonth.id, thisMonth]
-        ])
+        ]),
+        selectedPeriod: "Today"
     }),
 
     actions: {
-        // ...
+        setSelectedPeriod(period: Period) {
+            this.selectedPeriod = period
+        }
+    },
+
+    getters: {
+        filteredPosts: (state): TimelinePost[] => {
+            return state.ids
+                    .map(id => {  
+                        const post = state.all.get(id)
+
+                        if (!post) {
+                            throw Error(`Post with id of ${id} was expected but not found.`)
+                        }
+
+                        return {
+                            ...post,
+                            created: DateTime.fromISO(post.created)
+                        }    
+                    })
+                    .filter(post => {
+                        if (state.selectedPeriod === "Today") {
+                            return post.created >= DateTime.now().minus({ day: 1 })
+                        }
+
+                        if (state.selectedPeriod === "This Week") {
+                            return post.created >= DateTime.now().minus({ week: 1 })
+                        }
+                        
+                        return post 
+                    })
+        }
     }
 })
