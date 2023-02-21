cbdf31326a26d634019353f58a4632419b1312e8
Serhii Holub <serhii.holub@UA02273.local>
2023-02-21 12:34:08 +0200

    Two Way Binding with content editable

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -7,12 +7,22 @@ const props = defineProps<{
 }>()
 
 const title = ref(props.post.title)
+const content = ref(props.post.markdown)
 const contentEditable = ref<HTMLDivElement>()
 
 onMounted(() => {
-    console.log('Mounted')
-    console.log(contentEditable.value?.innerText)
+    if (!contentEditable.value) {
+        throw Error('ContentEditable DOM node was not found')
+    }
+    contentEditable.value.innerText = content.value
 })
+
+function handleInput () {
+    if (!contentEditable.value) {
+        throw Error('ContentEditable DOM node was not found')
+    }
+    content.value = contentEditable.value?.innerText
+}
 </script>
 
 <template>
@@ -21,11 +31,15 @@ onMounted(() => {
             <div class="field">
                 <div class="label">Post title</div>
                 <input type="text" class="input" v-model="title">
-                {{ title }}
             </div>
-            <div contenteditable ref="contentEditable">
-                This is the post.
-            </div>
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
+            <div contenteditable ref="contentEditable" @input="handleInput" />
+        </div>
+        <div class="column">
+            {{ content }}
         </div>
     </div>
 </template>
