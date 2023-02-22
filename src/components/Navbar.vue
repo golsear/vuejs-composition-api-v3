bce91f207b8630e6ac536e96e55ff390993d31c3
Serhii Holub <serhii.holub@UA02273.local>
2023-02-22 17:41:08 +0200

    Creating the useModal Composable

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,15 @@
 
-import { RouterLink } from 'vue-router';
+<script lang="ts" setup>
+import { useModal } from '../composables/modal'
+
+const modal = useModal()
+</script>>
 
 <template>
     <div class="navbar">
         <div class="navbar-end">
             <div class="buttons">
+                <button class="button" @click="modal.showModal()">Sign Up</button>
                 <RouterLink to="/posts/new" class="button">New Post</RouterLink>
             </div>
         </div>
