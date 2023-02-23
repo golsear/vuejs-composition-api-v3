f539c009ccbb8af06e97b3b32214974749061fee
Serhii Holub <serhii.holub@UA02273.local>
2023-02-23 13:57:59 +0200

    Framework Agnostic Validation - Part 2

--- src/validation.ts
+++ src/validation.ts
@@ -5,6 +5,17 @@ interface Status {
 
 type Rule = (value: string) => Status
 
+export function length ({ min, max }: { min: number, max: number }) {
+    return function (value: string): Status {
+        const result = Boolean(value.length > min && value.length < max)
+
+        return {
+            valid: result,
+            message: result ? undefined : `This fields must be between ${min} and ${max}` 
+        }
+    }
+}
+
 export function required (value: string): Status {
     const result = Boolean(value)
 
@@ -29,6 +40,7 @@ export function validate (value: string, rules: Rule[]): Status {
 }
 
 console.log(
-    validate('', [required]),
-    validate('username', [required])
+    validate('a', [length({ min: 5, max: 10 })]),
+    validate('aaaaaaa', [length({ min: 5, max: 10 })]),
+    validate('aaaaaaaaaaa', [length({ min: 5, max: 10 })])
 )
