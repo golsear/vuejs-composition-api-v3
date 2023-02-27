ae92f8a2574f50006d5761af1811bbe6cafcbc27
Serhii Holub <serhii.holub@UA02273.local>
2023-02-27 13:25:49 +0200

    Vue Router and Navigation Guards

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,25 @@
 
 <script lang="ts" setup>
+import { useRouter } from 'vue-router'
 import { useModal } from '../composables/modal'
 import { useUsers } from '../stores/users'
 
 const modal = useModal()
 const userStore = useUsers()
+const router = useRouter()
+
+async function logout() {
+    await userStore.logout()
+    router.push({ path: "/" })
+}    
 </script>>
 
 <template>
     <div class="navbar">
         <div class="navbar-end">
             <div v-if="userStore.currentUserId" class="buttons">
-                <button class="button" @click="userStore.logout()">Log Out</button>
                 <RouterLink to="/posts/new" class="button">New Post</RouterLink>
+                <button class="button" @click="logout()">Log Out</button>
             </div>
             
             <div v-else class="buttons">
