4a115728f2214d9c20ea682973007ad5b3fe49e3
Serhii Holub <serhii.holub@UA02273.local>
2023-03-01 11:03:59 +0200

    Node.js - PUT Endpoint for Updates

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -11,7 +11,7 @@ defineProps<{
     <RouterLink
         :to="`/posts/${post.id}`" 
         class="panel-block is-flex is-flex-direction-column is-align-items-flex-start">
-        <a>{{ post.title }}</a>
+        <a>{{ post.title }} {{ post.id }}</a>
         <div>{{ post.created.toFormat("d MMM") }}</div>
     </RouterLink>
 </template>
