360ac19952bd1c0c74e18e0dd25867b2e5225afa
Serhii Holub <serhii.holub@UA02273.local>
2023-03-06 10:25:19 +0200

    Mounting Component with Vue Test Utils

--- src/components/FormInput.spec.ts
+++ src/components/FormInput.spec.ts
@@ -1,8 +1,37 @@
 import { mount } from '@vue/test-utils'
-import { describe, it } from 'vitest'
+import { describe, it, expect } from 'vitest'
+import FormInput from './FormInput.vue'
 
 describe('FormInput', () => {
     it("renders some erros", () => {
-        
+        const wrapper = mount(FormInput, {
+            props: {
+                name: 'foo',
+                modelValue: 'bar',
+                status: {
+                    valid: false,
+                    message: 'error'
+                },
+                type: 'input'
+            }
+        })
+
+        expect(wrapper.find('.is-danger').exists()).toBe(true)
+    })
+
+    it("renders no erros", () => {
+        const wrapper = mount(FormInput, {
+            props: {
+                name: 'foo',
+                modelValue: 'bar',
+                status: {
+                    valid: true,
+                    message: 'error'
+                },
+                type: 'input'
+            }
+        })
+
+        expect(wrapper.find('.is-danger').exists()).toBe(false)
     })
 })
